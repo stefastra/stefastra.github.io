44d114ad9a257ecb31cafdacd4d6a178d9094bc3
Stefanos Strauss <stefastra@gmail.com>
2020-10-12 01:49:30 +0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,10 +1,15 @@
 function setup() {
-  createCanvas(800, 600,WEBGL);
+  createCanvas(displayWidth, displayHeight,WEBGL);
 }
 
 function draw() {
   background('sienna');
   rotateX(frameCount * 0.01);
   rotateY(frameCount * 0.01);
-  torus(30, 15);
+    if (mouseIsPressed) {
+    fill(0);
+  } else {
+    fill(255);
+  }
+  torus(mouseX, mouseY, 80, 80);
 }
